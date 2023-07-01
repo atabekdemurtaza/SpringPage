5077d3335fc2d90f1420b0ef5a146221358f9027
atabekdemurtaza <mychannel21war@gmail.com>
2023-07-01 17:57:24 +0500

    Second

--- config/settings.py
+++ config/settings.py
@@ -1,26 +1,28 @@
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 import os
-import environ
+#mport environ
 
-env = environ.Env(
+"""env = environ.Env(
     DEBUG=(bool, False)
-)
+)"""
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+"""# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))"""
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'u%&mewy2r!=alh=1(zx*+f9s$61_70)2l&vbcjfk8*0cwzreu-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+#DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
